e7df9be4efe15dea5f9827a26c3b511bef1d178e
mockmez <mockmez912@gmail.com>
2023-08-02 16:34:08 +0100

    Lombok annotations added

---
+++ src/main/java/com/Netsoc/place/placeUser/UserRole.java
@@ -0,0 +1,6 @@
+package com.Netsoc.place.placeUser;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
--- src/main/java/com/Netsoc/place/placeUser/appUser.java
+++ src/main/java/com/Netsoc/place/placeUser/appUser.java
@@ -1,44 +1,88 @@
 package com.Netsoc.place.placeUser;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
 
 public class appUser implements UserDetails {
 
+    @Id
+    @SequenceGenerator(
+            name = "user_seq",
+            sequenceName = "user_seq",
+            allocationSize = 1;
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_seq"
+    )
+    private Long userID;
+    private String username;
+    private String StudentID;
+    private String email;
+    private String password;
+    private UserRole userRole;
+    private boolean locked;
+    private boolean enabled;
+
+    public appUser(String username, String studentID, String email, String password, UserRole userRole, boolean locked, boolean enabled) {
+        this.username = username;
+        StudentID = studentID;
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singletonList(new SimpleGrantedAuthority(userRole.name()));
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return !locked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
     }
 }
